102710f45a079214e7ca89b8debeaf3f44e2b2c9
Le Minh Duc <duclm3.tt>
2021-02-20 22:55:32 +0700

    add MinArray

---
+++ 3. Mang va phuong thuc trong Java/BaiTap/src/MinArray.java
@@ -0,0 +1,12 @@
+public class MinArray {
+    public static void main(String[] args) {
+        int[] a = {13,8,2,4,0,10,-1};
+        int min = a[0];
+        for (int i = 0; i <a.length ; i++) {
+            if(min > a[i]){
+                min = a[i];
+            }
+        }
+        System.out.println(mins);
+    }
+}
